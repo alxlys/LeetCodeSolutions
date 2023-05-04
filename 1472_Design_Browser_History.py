e14c54ed2fc535213c1196d851af31452a92d9f8
lys <lys@gmail.com>
2023-05-04 19:53:01 +0300

    browser history

--- 1472_Design_Browser_History.py
+++ 1472_Design_Browser_History.py
@@ -9,13 +9,30 @@ class Node:
 class BrowserHistory:
 
     def __init__(self, homepage: str):
+        self.current = Node(homepage, None, None)
 
     def visit(self, url: str) -> None:
+        node = Node(url, None, self.current)
+        self.current.next = node
+        self.current = node
 
     def back(self, steps: int) -> str:
+        idx = 0
+        while idx != steps:
+            if not self.current.prev:
+                break
+            self.current = self.current.prev
+            idx += 1
+        return self.current.val
 
     def forward(self, steps: int) -> str:
-        pass
+        idx = 0
+        while idx != steps:
+            if not self.current.next:
+                break
+            self.current = self.current.next
+            idx += 1
+        return self.current.val
 
 
 if __name__ == '__main__':
@@ -23,12 +40,12 @@ if __name__ == '__main__':
     browserHistory.visit("google.com")  # You are in "leetcode.com".Visit "google.com"
     browserHistory.visit("facebook.com")  # You are in "google.com".Visit "facebook.com"
     browserHistory.visit("youtube.com")  # You are in "facebook.com".Visit "youtube.com"
-    browserHistory.back(1)  # You are in "youtube.com", move back to "facebook.com" return "facebook.com"
-    browserHistory.back(1)  # You are in "facebook.com", move back to "google.com" return "google.com"
-    browserHistory.forward(1)  # You are in "google.com", move forward to "facebook.com" return "facebook.com"
+    print(browserHistory.back(1))  # You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+    print(browserHistory.back(1))  # You are in "facebook.com", move back to "google.com" return "google.com"
+    print(browserHistory.forward(1))  # You are in "google.com", move forward to "facebook.com" return "facebook.com"
     browserHistory.visit("linkedin.com")  # You are in "facebook.com".Visit "linkedin.com"
-    browserHistory.forward(2)  # You are in "linkedin.com", you cannot move forward any steps.
-    browserHistory.back(
-        2)  # You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
-    browserHistory.back(
-        7)  # You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+    print(browserHistory.forward(2))  # You are in "linkedin.com", you cannot move forward any steps.
+    print(browserHistory.back(
+        2))  # You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+    print(browserHistory.back(
+        7))  # You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
