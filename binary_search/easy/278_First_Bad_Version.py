cf98a09cedf8794f51b7593a6aa2c1955e18d50a
lys <lys@gmail.com>
2023-05-13 17:29:33 +0300

    optimize first bad version

--- binary_search/easy/278_First_Bad_Version.py
+++ binary_search/easy/278_First_Bad_Version.py
@@ -18,7 +18,9 @@ class Solution:
         while left < right:
             mid = left + (right - left) // 2
             if isBadVersion(mid):
-                right = mid
+                if not isBadVersion(mid - 1):
+                    return mid
+                right = mid - 1
             else:
                 left = mid + 1
 
